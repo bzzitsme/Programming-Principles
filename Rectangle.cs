cf20dae2bf5f760f8a351900dd98891a67ddf36b
bzzitsme <35283270+bzzitsme@users.noreply.github.com>
2018-01-13 20:34:07 +0600

    add lab

---
+++ Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Rectangle
+{
+    public int Width, Length, Area, Perimetr;
+    public static float FindArea(int w, int l)
+    {
+        return w * l;
+    }
+    public static int FindPerimetr(int w, int l)
+    {
+        return w + l + w + l;
+    }
+    public Rectangle()
+    {
+        Width = 0;
+        Length = 0;
+    }
+    public Rectangle(int w, int l)
+    {
+        Width = w;
+        Length = l;
+        Area = FindArea(w, l);
+        Perimetr = FindPerimetr(w, l);
+    }
+    public override string ToString()
+    {
+        return Width + " " + Length + " " + Perimetr + " " + Area;
+    }
+}
