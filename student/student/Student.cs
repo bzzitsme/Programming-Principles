516db7afce3dfe650b7322c415f7d499d5ee98fa
bzzitsme <35283270+bzzitsme@users.noreply.github.com>
2018-01-13 20:17:16 +0600

    add lab
    class student

---
+++ student/student/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    public class Student
+    {
+        public string name;
+        public int age;
+        public double gpa;
+
+        public Student(string n, int a, double g)
+        {
+            name = n;
+            age = a;
+            gpa = g;
+        }
+        public override string ToString()
+        {
+            return name + " " + age + " " + gpa;
+        }
+    }
+}
