c0b2260af8b2f0301e536606453501ee2d9c9532
unknown <bzzitsme@gmail.com>
2018-01-17 10:23:16 +0600

    add lab

---
+++ Lab1Circle/Lab1Circle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Circle
+{
+    class Circle
+    {
+        double radius, diameter, circumference, area;
+        public Circle(double  x)
+        {
+            radius = x;
+            FindArea(radius);
+            FindCircumference(radius);
+            FindDiameter(radius);
+        }
+        public void FindArea(double x)
+        {
+            area = Math.PI * radius * radius;
+        }
+        public void FindDiameter(double x)
+        {
+            diameter = 2 * radius;
+        }
+        public void FindCircumference(double x)
+        {
+            circumference = Math.PI * 2 * radius;
+        }
+        public override string ToString()
+        {
+            return area + " " + diameter + " " + circumference;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double q = double.Parse(Console.ReadLine());
+            Circle a = new Circle(q);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
