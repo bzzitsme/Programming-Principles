afcf00ab610d2f3c703e1197bddafb5155d296c9
BzzItsMe <bzzitsme@gmail.com>
2018-03-14 20:55:38 +0600

    add lab

---
+++ KorobkiSNeba/KorobkiSNeba/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace KorobkiSNeba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 25;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(26, 301);
+            this.trackBar1.Maximum = 245;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(255, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(292, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(12, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "You";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 346);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ KorobkiSNeba/KorobkiSNeba/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KorobkiSNeba
+{
+    public partial class Form1 : Form
+    {
+        int dx = 1;
+        bool test = true;
+        Button button = new Button();
+        Random rnd = new Random();
+        List<Button> vector = new List<Button>();
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int xx = rnd.Next(0, Width - (Width - 275));
+            int yy = rnd.Next(0, 40);
+            Button btn1 = new Button();
+            btn1.Location = new Point(xx, yy);
+            btn1.Size = new Size(35, 35);
+            btn1.Text = "#";
+            if(test)
+            {
+
+                vector.Add(btn1);
+                Controls.Add(btn1);
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(test)
+            {
+                for(int i = 0; i < vector.Count(); i++)
+                {
+                    vector[i].Location = new Point(vector[i].Location.X, vector[i].Location.Y + 3);
+                    if (vector[i].Location.Y == 300)
+                    {
+                        vector[i].Hide();
+                        vector.RemoveAt(i);
+                        score += 1;
+                        label1.Text = "Score " + score;
+                    }
+                    else if (vector[i].Location.Y > button1.Location.Y + 35)
+                    {
+                        vector[i].Hide();
+                        vector.RemoveAt(i);
+                    }
+                    else if (Math.Abs(vector[i].Location.Y - button1.Location.Y) <= 35 && Math.Abs(vector[i].Location.X - button1.Location.X) <= 35)
+                    {
+                        timer1.Stop();
+                        timer2.Stop();
+                        MessageBox.Show("Game Over noob");
+                        test = false;
+                    }
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (test)
+            {
+                button1.Location = new Point(trackBar1.Value, button1.Location.Y);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /* if (e.KeyChar.ToString() == "A")
+            {
+                x -= dx;
+                button.Location = new Point(x, y);
+            }
+            if (e.KeyChar.ToString() == "D")
+            {
+                x += dx;
+                button.Location = new Point(x, y);
+            } */
+        }
+    }
+}
